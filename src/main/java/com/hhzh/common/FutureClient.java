cc04b79e4a6d889e088fcca9901df2d8310dfe37
hehuazhen <huazhen.he@okcoin.com>
2018-07-20 20:32:12 +0800

    format code

--- src/main/java/com/hhzh/common/FutureClient.java
+++ src/main/java/com/hhzh/common/FutureClient.java
@@ -17,13 +17,13 @@ public class FutureClient {
     public static void main(String[] args) throws HttpException, IOException {
 
 
-        String api_key = "";  //OKCoin申请的apiKey
-        String secret_key = "";  //OKCoin申请的secretKey
-        String url_prex = "";  //注意：请求URL 国际站https://www.okcoin.com ; 国内站https://www.okcoin.cn
+        String apiKey = "";  //OKCoin申请的apiKey
+        String secretKey = "";  //OKCoin申请的secretKey
+        String urlPrex = "";  //注意：请求URL 国际站https://www.okcoin.com ; 国内站https://www.okcoin.cn
         /**
          *  get请求无需发送身份认证,通常用于获取行情，市场深度等公共信息
          */
-        IFutureTrade futureGetV1 = new FutureTradeImpl(url_prex);
+        IFutureTrade futureGetV1 = new FutureTradeImpl(urlPrex);
 
         /**
          * post请求需发送身份认证，获取用户个人相关信息时，需要指定api_key,与secret_key并与参数进行签名，
@@ -31,7 +31,7 @@ public class FutureClient {
          * 发送post请求之前，程序会做自动加密，生成签名。
          *
          */
-        IFutureTrade futurePostV1 = new FutureTradeImpl(url_prex, api_key,secret_key);
+        IFutureTrade futurePostV1 = new FutureTradeImpl(urlPrex, apiKey, secretKey);
 
         //期货行情信息
         futureGetV1.futureTicker("btc_usd", "this_week");
@@ -49,16 +49,16 @@ public class FutureClient {
         futureGetV1.exchangeRate();
 
         //期货下单
-        String tradeResultV1 = futurePostV1.futureTrade("btc_usd","this_week", "10.134", "1", "1", "0");
+        String tradeResultV1 = futurePostV1.futureTrade("btc_usd", "this_week", "10.134", "1", "1", "0");
         JSONObject tradeJSV1 = JSONObject.parseObject(tradeResultV1);
         String tradeOrderV1 = tradeJSV1.getString("order_id");
         System.out.println(tradeResultV1);
 
         //期货用户订单查询
-        futurePostV1.futureOrderInfo("btc_usd", "this_week",tradeOrderV1, "1", "1", "2");
+        futurePostV1.futureOrderInfo("btc_usd", "this_week", tradeOrderV1, "1", "1", "2");
 
         //取消订单
-        futurePostV1.futureCancel("btc_usd", "this_week",tradeOrderV1);
+        futurePostV1.futureCancel("btc_usd", "this_week", tradeOrderV1);
 
         //期货账户信息
         futurePostV1.futureUserinfo();
